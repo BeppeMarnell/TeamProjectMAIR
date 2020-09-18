49cbcade4a493441744473fc2eb2d4aa66625131
nadbot <Daniel.Hamandouche@gmx.net>
2020-09-18 20:07:11 +0200

    Added functionality for almost all utterances

--- src/Chat/ChatManager.py
+++ src/Chat/ChatManager.py
@@ -22,6 +22,7 @@ class ChatManager:
 
         # Load the System utterances
         self.sys_utter = {}
+        self.print_text = ""
 
         with open('assets/sys_utterances.txt', 'r') as inf:
             for line in inf:
@@ -40,6 +41,15 @@ class ChatManager:
             # Check system state and preferences
             self.SystemStateUtterance()
 
+            if self.state == State.S1:
+                # Define preferences
+                pref = {
+                    'pricerange': '',
+                    'area': '',
+                    'food': ''
+                }
+                self.pref_df = pd.DataFrame(pref, index=[0])
+
             # S6 is the end state, therefore terminate the program
             if self.state == State.S6:
                 break
@@ -50,29 +60,10 @@ class ChatManager:
             # Evaluate inputted utterance and check the next state
             utterance = self.models.evalueNewUtterance(user_input)
 
-            # Update preferences and state
-            new_preferences = self.models.extractPreference(user_input)
-
             # TODO: for now basic talking stuff
-            # Change preferences where necessary
-            if new_preferences['food'] != '':
-                self.pref_df.at[0, 'food'] = new_preferences['food']
-                self.models.restaurants = []
 
-            if new_preferences['area'] != '':
-                self.pref_df.at[0, 'area'] = new_preferences['area']
-                self.models.restaurants = []
+            self.react_to_utterance(utterance, user_input)
 
-            if new_preferences['pricerange'] != '':
-                self.pref_df.at[0, 'pricerange'] = new_preferences['pricerange']
-                self.models.restaurants = []
-
-            # evaluate the utterance if the user asks for more information (phone number, address, ...)
-            if utterance == 'request' and self.state == State.S5:
-                details = self.models.extract_details(user_input)
-                for element in details:
-                    print(self.sys_utter['details'].replace('detail_type', element[0])
-                          .replace('detail_info', element[1]))
             # Update the system state
             # if new_state == State.S4 and self.state == State.S3 and
 
@@ -87,7 +78,7 @@ class ChatManager:
             print('')
             print('utterance: ')
             print(utterance)
-            print(new_preferences)
+            # print(new_preferences)
             print('preference: ')
             print(self.pref_df.loc[0])
             print('current state: ')
@@ -105,31 +96,193 @@ class ChatManager:
 
         # Check the state
         if self.state == State.S1:
-            print(self.sys_utter['state1'])
+            self.print_text = self.sys_utter['state1']
+            print(self.print_text)
             return
 
         if self.state == State.S2 and not food:
-            print(self.sys_utter['askfood'])
+            self.print_text = self.sys_utter['askfood']
+            print(self.print_text)
             return
 
         if self.state == State.S2 and not area:
-            print(self.sys_utter['askarea'])
+            self.print_text = self.sys_utter['askarea']
+            print(self.print_text)
             return
 
         if self.state == State.S2 and not price:
-            print(self.sys_utter['askprice'])
+            self.print_text = self.sys_utter['askprice']
+            print(self.print_text)
             return
 
         if self.state == State.S4:
             if len(self.models.recommendation) == 0:
-                print(self.sys_utter['noresults'])
+                self.print_text = self.sys_utter['noresults']
+
             elif len(self.models.recommendation) == 1 and self.models.recommendation[0] == -1:
-                print(self.sys_utter['nomoreresults'])
+                self.print_text = self.sys_utter['nomoreresults']
             else:
-                print(self.sys_utter['suggestrest'].replace('restaurant_name',
-                                                            self.models.recommendation['restaurantname']))
+                self.print_text = self.sys_utter['suggestrest'].replace('restaurant_name',
+                                                                        self.models.recommendation['restaurantname'])
+            print(self.print_text)
             return
 
         if self.state == State.S6:
-            print(self.sys_utter['bye'])
+            self.print_text = self.sys_utter['bye']
+            print(self.print_text)
             return
+
+    def react_to_utterance(self, utterance, user_input):
+        if utterance == 'ack':
+            # same as affirm
+            if self.state == State.S4:
+                self.print_text = self.sys_utter['affirm'].replace('restaurant_name',
+                                                                   self.models.recommendation['restaurantname'])
+                print(self.print_text)
+                return
+
+        if utterance == 'affirm':
+            # same as ack
+            if self.state == State.S4:
+                self.print_text = self.sys_utter['affirm'].replace('restaurant_name',
+                                                                   self.models.recommendation['restaurantname'])
+                print(self.print_text)
+                return
+
+        if utterance == 'bye':
+            # don't need to do anything as it will automatically move to state S6 and print goodbye message
+            return
+
+        if utterance == 'confirm':
+            # Update preferences and state
+            new_preferences = self.models.extractPreference(user_input)
+            #print(new_preferences)
+            # check what preference to confirm
+            food = True if new_preferences['food'] != '' else False
+            area = True if new_preferences['area'] != '' else False
+            price = True if new_preferences['pricerange'] != '' else False
+
+            # check if the preference is correct
+            food_correct = True if self.pref_df['food'].tolist()[0] == new_preferences['food'] else False
+            area_correct = True if self.pref_df['area'].tolist()[0] == new_preferences['area'] else False
+            price_correct = True if self.pref_df['pricerange'].tolist()[0] == new_preferences['pricerange'] else False
+
+            # check that preference is not empty
+            food_given = True if self.pref_df['food'].tolist()[0] != '' else False
+            area_given = True if self.pref_df['area'].tolist()[0] != '' else False
+            price_given = True if self.pref_df['pricerange'].tolist()[0] != '' else False
+
+            if food:
+                if food_correct:
+                    print(self.sys_utter['confirmtrue'].replace('preference_name', 'restaurant').
+                          replace('preference_value', self.pref_df['food'].tolist()[0]))
+                elif food_given:
+                    print(self.sys_utter['confirmfalse'].replace('preference_name', 'restaurant').
+                          replace('preference_value', self.pref_df['food'].tolist()[0]))
+                else:
+                    print(self.sys_utter['confirmfalse'].replace('preference_name', 'type of restaurant').
+                          replace('preference_value', 'not specified'))
+
+            if area:
+                if area_correct:
+                    print(self.sys_utter['confirmtrue'].replace('preference_name', 'area').
+                          replace('preference_value', self.pref_df['area'].tolist()[0]))
+                elif area_given:
+                    print(self.sys_utter['confirmfalse'].replace('preference_name', 'area').
+                          replace('preference_value', self.pref_df['area'].tolist()[0]))
+                else:
+                    print(self.sys_utter['confirmfalse'].replace('preference_name', 'area').
+                          replace('preference_value', 'not specified'))
+
+            if price:
+                if price_correct:
+                    print(self.sys_utter['confirmtrue'].replace('preference_name', 'price').
+                          replace('preference_value', self.pref_df['pricerange'].tolist()[0]))
+                elif price_given:
+                    print(self.sys_utter['confirmfalse'].replace('preference_name', 'price').
+                          replace('preference_value', self.pref_df['pricerange'].tolist()[0]))
+                else:
+                    print(self.sys_utter['confirmfalse'].replace('preference_name', 'price').
+                          replace('preference_value', 'not specified'))
+            return
+
+        if utterance == 'deny':
+            # TODO
+            pass
+
+        if utterance == 'hello':
+            # don't need to do anything, will automatically restart the conversation
+            return
+
+        if utterance == 'inform':
+            # Update preferences and state
+            new_preferences = self.models.extractPreference(user_input)
+            # Change preferences where necessary
+            if new_preferences['food'] != '':
+                self.pref_df.at[0, 'food'] = new_preferences['food']
+                self.models.restaurants = []
+
+            if new_preferences['area'] != '':
+                self.pref_df.at[0, 'area'] = new_preferences['area']
+                self.models.restaurants = []
+
+            if new_preferences['pricerange'] != '':
+                self.pref_df.at[0, 'pricerange'] = new_preferences['pricerange']
+                self.models.restaurants = []
+            return
+
+        if utterance == 'negate':
+            # TODO
+            pass
+
+        if utterance == 'null':
+            # don't need to do anything, as the text for misunderstanding will be printed if command was not understood
+            # important: use pass not return here to let it print sys_utter['misunderstanding']
+            pass
+
+        if utterance == 'repeat':
+            print(self.print_text)
+            return
+
+        if utterance == 'reqalts':
+            # same as inform
+            new_preferences = self.models.extractPreference(user_input)
+            # Change preferences where necessary
+            if new_preferences['food'] != '':
+                self.pref_df.at[0, 'food'] = new_preferences['food']
+                self.models.restaurants = []
+
+            if new_preferences['area'] != '':
+                self.pref_df.at[0, 'area'] = new_preferences['area']
+                self.models.restaurants = []
+
+            if new_preferences['pricerange'] != '':
+                self.pref_df.at[0, 'pricerange'] = new_preferences['pricerange']
+                self.models.restaurants = []
+            return
+
+        if utterance == 'reqmore':
+            # don't need to do anything, as it is already updated in the run method (if new state == S4)
+            return
+
+        if utterance == 'request':
+            # evaluate the utterance if the user asks for more information (phone number, address, ...)
+            if self.state == State.S5:
+                details = self.models.extract_details(user_input)
+                for element in details:
+                    print(self.sys_utter['details'].replace('detail_type', element[0])
+                          .replace('detail_info', element[1]))
+                return
+
+        if utterance == 'restart':
+            self.print_text = self.sys_utter['restart']
+            print(self.print_text)
+            return
+
+        if utterance == 'thankyou':
+            # don't need to do anything, as it will automatically move to final state and print bye utterance
+            return
+
+        self.print_text = self.sys_utter['misunderstanding']
+        print(self.print_text)
+        return
--- src/Chat/StateManager.py
+++ src/Chat/StateManager.py
@@ -16,28 +16,35 @@ class StateManager:
 
         self.__states = [s1, s2, s3, s4, s5, s6]
 
-        e1 = Edge(s2, ['inform', 'reqalts', 'negate'])
-        e2 = Edge(s1, ['confirm', 'affirm', 'request', 'thankyou', 'null', 'bye', 'hello', 'repeat', 'ack', 'restart',
-                       'deny', 'reqmore'])
-        s1.addEdge(e1)
-        s1.addEdge(e2)
+        e12 = Edge(s2, ['inform', 'reqalts', 'negate', 'deny', 'reqmore'])
+        e11 = Edge(s1, ['confirm', 'affirm', 'request', 'null', 'hello', 'repeat', 'ack', 'restart'])
+        e14 = Edge(s4, ['_allinfo'])
+        e16 = Edge(s6, ['bye', 'thankyou'])
+        s1.addEdge(e12)
+        s1.addEdge(e11)
+        s1.addEdge(e14)
+        s1.addEdge(e16)
 
         # TODO: come up with a way this works. State2 is a transition node where the preference are checked
-        # TODO add possibility to restart
-        e2 = Edge(s2, ['_missinginfo'])
+        # TODO add possibility to restart and say bye
+        e22 = Edge(s2, ['confirm', 'affirm', 'request', 'null', 'repeat', 'ack', 'deny',
+                        'reqmore', 'inform', 'reqalts', 'negate'])
+        e21 = Edge(s1, ['restart', 'hello'])
+        e26 = Edge(s6, ['bye', 'thankyou'])
         # e3 = Edge(s3, ['_missinginfo'])
-        e4 = Edge(s4, ['_allinfo'])
-        s2.addEdge(e2)
-        # s2.addEdge(e3)
-        s2.addEdge(e4)
+        e24 = Edge(s4, ['_allinfo'])
+        s2.addEdge(e22)
+        s2.addEdge(e21)
+        s2.addEdge(e24)
+        s2.addEdge(e26)
 
         # TODO remove null (as that can mean not understood)
-        e5 = Edge(s3, ['confirm', 'affirm', 'request', 'thankyou', 'null', 'bye', 'repeat', 'ack', 'deny', 'reqmore', 'hello'])
-        e6 = Edge(s2, ['inform', 'reqalts', 'negate'])
-        e7 = Edge(s1, ['restart'])
-        s3.addEdge(e5)
-        s3.addEdge(e6)
-        s3.addEdge(e7)
+        #e33 = Edge(s3, ['confirm', 'affirm', 'request', 'thankyou', 'null', 'bye', 'repeat', 'ack', 'deny', 'reqmore', 'hello'])
+        #e32 = Edge(s2, ['inform', 'reqalts', 'negate'])
+        #e31 = Edge(s1, ['restart'])
+        #s3.addEdge(e33)
+        #s3.addEdge(e32)
+        #s3.addEdge(e31)
 
         # TODO if yes moves to state 5, then you cannot say yes, then ask for more infos.
         #  Need state 6 as final state, only if you say goodbye or thank you
@@ -48,44 +55,45 @@ class StateManager:
         # e9 = Edge(s5, ['ack', 'affirm', 'null', 'bye', 'thankyou'])
         # e10 = Edge(s1, ['restart', 'hello'])
         # TODO move confirm and inform and ... to e9?
-        e8 = Edge(s4, ['confirm', 'deny', 'inform', 'negate', 'reqmore', 'reqalts'])
-        e9 = Edge(s5, ['request', 'repeat', 'ack', 'affirm', 'null', 'thankyou'])
-        e10 = Edge(s1, ['restart', 'hello'])
-        e11 = Edge(s6, ['bye'])
-        s4.addEdge(e8)
-        s4.addEdge(e9)
-        s4.addEdge(e10)
-        s4.addEdge(e11)
+        e42 = Edge(s2, ['inform'])
+        e44 = Edge(s4, ['confirm', 'repeat', 'deny', 'negate', 'reqmore', 'reqalts'])
+        e45 = Edge(s5, ['request', 'ack', 'affirm', 'null'])
+        e41 = Edge(s1, ['restart', 'hello'])
+        e46 = Edge(s6, ['bye', 'thankyou'])
+        s4.addEdge(e42)
+        s4.addEdge(e44)
+        s4.addEdge(e45)
+        s4.addEdge(e41)
+        s4.addEdge(e46)
 
-        e12 = Edge(s4, ['confirm', 'deny', 'inform', 'negate', 'reqmore', 'reqalts'])
-        e13 = Edge(s5, ['request', 'repeat', 'ack', 'affirm', 'null'])
-        e14 = Edge(s1, ['restart', 'hello'])
-        e15 = Edge(s6, ['bye', 'thankyou'])
-        s5.addEdge(e12)
-        s5.addEdge(e13)
-        s5.addEdge(e14)
-        s5.addEdge(e15)
+        e54 = Edge(s4, ['deny', 'inform', 'negate', 'reqmore', 'reqalts'])
+        e55 = Edge(s5, ['confirm', 'request', 'repeat', 'ack', 'affirm', 'null'])
+        e51 = Edge(s1, ['restart', 'hello'])
+        e56 = Edge(s6, ['bye', 'thankyou'])
+        s5.addEdge(e54)
+        s5.addEdge(e55)
+        s5.addEdge(e51)
+        s5.addEdge(e56)
 
     def processState(self, state, utterance, preferences):
         # All the possibilities should be here, based on the utterance,
         # the function will return the next state
 
-        #TODO: add special case, when in state S2,
-        # then check for preferences before going on
+        # TODO: add special case, when in state S2,
+        #  then check for preferences before going on
 
         for s in self.__states:
             if s.state == state:
                 # special case if state is S2
-                if s.state == State.S2:
-                    print(preferences)
+                if s.state == State.S2 or s.state == State.S1:
                     food = True if preferences['food'].tolist()[0] != '' else False
                     area = True if preferences['area'].tolist()[0] != '' else False
                     price = True if preferences['pricerange'].tolist()[0] != '' else False
                     if food and area and price:
                         utterance = "_allinfo"
-                    else:
-                        utterance = "_missinginfo"
-                print(utterance)
+                    # TODO if I don't use elif block, i should be able to handle all utterances
+                    # elif s.state == State.S2:
+                    #     utterance = "_missinginfo"
                 return s.solve(utterance)
 
         return None
