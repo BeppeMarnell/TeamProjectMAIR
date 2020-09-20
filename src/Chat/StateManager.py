5cb2ab1cf6bf7a4ca4f45a46815713d975489ebf
Sinie <sinievdben@live.nl>
2020-09-20 08:34:51 +0200

    Merg2

--- src/Chat/StateManager.py
+++ src/Chat/StateManager.py
@@ -4,6 +4,8 @@ from enum import Enum
 class StateManager:
 
     def __init__(self):
+        #TODO: update the graph according to graph in Teams
+
         # Create the graph
         #TODO: update the graph according to the graph in teams
         s1 = StateNode(State.S1)
@@ -14,38 +16,62 @@ class StateManager:
 
         self.__states = [s1, s2, s3, s4, s5]
 
-        e1 = Edge(s2, ['inform', 'reqalts', 'negate'])
-        e2 = Edge(s1, ['confirm', 'affirm', 'request', 'thankyou', 'null', 'bye', 'hello', 'repeat', 'ack', 'restart',
-                       'deny', 'reqmore'])
-        s1.addEdge(e1)
-        s1.addEdge(e2)
+        e12 = Edge(s2, ['inform', 'reqalts', 'negate', 'deny', 'reqmore'])
+        e11 = Edge(s1, ['confirm', 'affirm', 'request', 'null', 'hello', 'repeat', 'ack', 'restart', 'thankyou'])
+        e13 = Edge(s3, ['_allinfo'])
+        e15 = Edge(s5, ['bye'])
+        s1.addEdge(e12)
+        s1.addEdge(e11)
+        s1.addEdge(e13)
+        s1.addEdge(e15)
 
-        e3 = Edge(s3, ['confirm', 'affirm', 'request', 'thankyou', 'null', 'bye', 'hello', 'repeat', 'ack', 'restart',
-                       'deny', 'reqmore'])
-        e4 = Edge(s4, ['inform', 'reqalts', 'negate'])
-        s2.addEdge(e3)
-        s2.addEdge(e4)
+        e22 = Edge(s2, ['confirm', 'affirm', 'request', 'null', 'repeat', 'ack', 'deny',
+                        'reqmore', 'inform', 'reqalts', 'negate', 'thankyou'])
+        e21 = Edge(s1, ['restart', 'hello'])
+        e25 = Edge(s5, ['bye'])
+        e23 = Edge(s3, ['_allinfo'])
+        s2.addEdge(e22)
+        s2.addEdge(e21)
+        s2.addEdge(e23)
+        s2.addEdge(e25)
 
-        e5 = Edge(s3, ['confirm', 'affirm', 'request', 'thankyou', 'null', 'bye', 'repeat', 'ack', 'deny', 'reqmore'])
-        e6 = Edge(s4, ['inform', 'reqalts', 'negate'])
-        e7 = Edge(s1, ['restart', 'hello'])
-        s3.addEdge(e5)
-        s3.addEdge(e6)
-        s3.addEdge(e7)
+        # if yes moves to state 5, then you cannot say yes, then ask for more infos.
+        # Need state 6 as final state, only if you say goodbye or thank you
+        # State 5: restaurant is pretty safe, (has been ack or more details have been requested)
+        #  if alternative is asked, switch to S4
+        e31 = Edge(s1, ['restart', 'hello'])
+        e32 = Edge(s2, ['inform'])
+        e33 = Edge(s3, ['confirm', 'repeat', 'deny', 'negate', 'reqmore', 'reqalts', 'thankyou'])
+        e34 = Edge(s4, ['request', 'ack', 'affirm', 'null'])
+        e35 = Edge(s5, ['bye'])
+        s3.addEdge(e31)
+        s3.addEdge(e32)
+        s3.addEdge(e33)
+        s3.addEdge(e34)
+        s3.addEdge(e35)
 
-        e8 = Edge(s4, ['confirm', 'deny', 'inform', 'negate', 'request', 'repeat', 'reqmore', 'reqalts'])
-        e9 = Edge(s5, ['ack', 'affirm', 'bye', 'null', 'thankyou'])
-        e10 = Edge(s1, ['restart', 'hello'])
-        s4.addEdge(e8)
-        s4.addEdge(e9)
-        s4.addEdge(e10)
+        e43 = Edge(s3, ['deny', 'inform', 'negate', 'reqmore', 'reqalts'])
+        e44 = Edge(s4, ['confirm', 'request', 'repeat', 'ack', 'affirm', 'null'])
+        e41 = Edge(s1, ['restart', 'hello'])
+        e45 = Edge(s5, ['bye', 'thankyou'])
+        s4.addEdge(e43)
+        s4.addEdge(e44)
+        s4.addEdge(e41)
+        s4.addEdge(e45)
 
-    def processState(self, state, utterance):
+    def processState(self, state, utterance, preferences):
         # All the possibilities should be here, based on the utterance,
         # the function will return the next state
 
         for s in self.__states:
             if s.state == state:
+                # special case if state is S2
+                if s.state == State.S2 or s.state == State.S1:
+                    food = True if preferences['food'].tolist()[0] != '' else False
+                    area = True if preferences['area'].tolist()[0] != '' else False
+                    price = True if preferences['pricerange'].tolist()[0] != '' else False
+                    if food and area and price:
+                        utterance = "_allinfo"
                 return s.solve(utterance)
 
         return None
@@ -70,7 +96,6 @@ class StateNode:
 
     def solve(self, utterance):
         # Look at the utterance and return the child node
-
         for edge in self.edges:
             if utterance in edge.utterances:
                 return edge.end.state
@@ -81,14 +106,7 @@ class StateNode:
 class Edge:
 
     def __init__(self, end, utterances):
+        # Attach to the end node
         self.end = end
         # List with the possible utterances in the edge
         self.utterances = utterances
-
-
-# ['inform', 'reqalts', 'confirm', 'affirm', 'request', 'thankyou', 'null', 'bye', 'negate', 'hello', 'repeat', 'ack', 'restart', 'deny', 'reqmore']
-
-if __name__ == '__main__':
-    s_m = StateManager()
-
-    print(s_m.processState(State.S1, 'ack'))
