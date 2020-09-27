784f33aa3ad187679c0f7c806272765c3a84ed02
nadbot <Daniel.Hamandouche@gmx.net>
2020-09-27 10:37:08 +0200

    Added priorities and iterations for rules

--- src/rules.py
+++ src/rules.py
@@ -45,6 +45,15 @@ class Rules:
             'fast food': '',
             'vegetarian': ''
         }
+        # numbers indicating the priority of the rule. A higher number means that it is more important
+        self.priorities = {
+            'busy': 0,
+            'long time': 0,
+            'children': 0,
+            'romantic': 0,
+            'fast food': 0,
+            'vegetarian': 0
+        }
         # self.consequents_df = pd.DataFrame(self.consequents, index=[0])
 
     def extract_implications(self, string):
@@ -59,10 +68,8 @@ class Rules:
         print(self.asked_consequents)
         return self.asked_consequents
 
-    def solve_rule(self, restaurant):
-        # TODO set priorities of rules
-        # print(self.consequents)
-        # print(restaurant['pricerange'])
+    def __solve_rule(self, restaurant, consequents):
+        # set antedecents known from the data set
         cheap = True if restaurant['pricerange'] == 'cheap' else False
         good = True if restaurant['foodquality'] == 'good' else False
         spanish = True if restaurant['food'] == 'spanish' else False
@@ -70,81 +77,122 @@ class Rules:
         expensive = True if restaurant['pricerange'] == 'expensive' else False
         international = True if restaurant['food'] == 'international' else False
         steakhouse = True if (restaurant['food'] == 'steakhouse' \
-                             or 'steakhouse' in restaurant['restaurantname']) else False
+                              or 'steakhouse' in restaurant['restaurantname']) else False
 
-        if self.consequents['busy'] is None:
-            self.consequents['busy'] = True if cheap and good else None
-            if self.consequents['busy']:
+        # copy status from previous iteration
+        new_consequents = consequents.copy()
+
+        # check if previous consequence is either empty or current priority is higher
+        if consequents['busy'] is None or 1 > self.priorities['busy']:
+            # update new consequence
+            new_consequents['busy'] = True if cheap and good else new_consequents['busy']
+            # check if new consequence was updated
+            # don't use if new_consequents['busy'] == True as that can be true from previous rules in this iteration
+            if cheap and good:
+                # update priority and set reason
+                self.priorities['busy'] = 1
                 self.reason['busy'] = "It is busy because it is good and cheap"
 
-        if self.consequents['long time'] is None:
-            self.consequents['long time'] = True if spanish else None
-            if self.consequents['long time']:
+        if consequents['long time'] is None or 1 > self.priorities['long time']:
+            new_consequents['long time'] = True if spanish else new_consequents['long time']
+            if spanish:
+                self.priorities['long time'] = 1
                 self.reason['long time'] = "It takes a long time because it is spanish food"
 
-        if self.consequents['long time'] is None:
-            self.consequents['long time'] = True if self.consequents['busy'] is True else None
-            if self.consequents['long time']:
+        if consequents['long time'] is None or 3 > self.priorities['long time']:
+            new_consequents['long time'] = True if consequents['busy'] is True else new_consequents['long time']
+            if consequents['busy'] is True:
+                self.priorities['long time'] = 3
                 self.reason['long time'] = "It takes a long time because it is a busy restaurant"
 
-        if self.consequents['children'] is None:
-            self.consequents['children'] = False if self.consequents['long time'] is True else None
-            if self.consequents['children'] is False:
+        if consequents['children'] is None or 2 > self.priorities['children']:
+            new_consequents['children'] = False if consequents['long time'] is True else new_consequents['children']
+            if consequents['long time'] is True:
+                self.priorities['children'] = 2
                 self.reason['children'] = "It is not recommended with children because it takes long"
 
-        if self.consequents['romantic'] is None:
-            self.consequents['romantic'] = False if self.consequents['busy'] is True else None
-            if self.consequents['romantic'] is False:
+        if consequents['romantic'] is None or 2 > self.priorities['romantic']:
+            new_consequents['romantic'] = False if consequents['busy'] is True else new_consequents['romantic']
+            if consequents['busy'] is True:
+                self.priorities['romantic'] = 2
                 self.reason['romantic'] = "It is not romantic because it is busy"
 
-        if self.consequents['romantic'] is None:
-            self.consequents['romantic'] = True if self.consequents['long time'] is True else None
-            if self.consequents['romantic']:
+        if consequents['romantic'] is None or 1 > self.priorities['romantic']:
+            new_consequents['romantic'] = True if consequents['long time'] is True else new_consequents['romantic']
+            if consequents['long time']:
+                self.priorities['romantic'] = 1
                 self.reason['romantic'] = "It is romantic because you usually spend a long time there"
 
         # Own rules
-        if self.consequents['busy'] is None:
-            self.consequents['busy'] = False if cheap and not centre else None
-            if self.consequents['busy'] is False:
+        if consequents['busy'] is None or 3 > self.priorities['busy']:
+            new_consequents['busy'] = False if cheap and not centre else new_consequents['busy']
+            if cheap and not centre:
+                self.priorities['busy'] = 3
                 self.reason['busy'] = "Since the restaurant is not in the centre, it is not busy"
 
-        if self.consequents['long time'] is None:
-            self.consequents['long time'] = True if expensive else None
-            if self.consequents['long time']:
+        if consequents['long time'] is None or 2 > self.priorities['long time']:
+            new_consequents['long time'] = True if expensive else new_consequents['long time']
+            if expensive:
+                self.priorities['long time'] = 2
                 self.reason['long time'] = "Since the restaurant is expensive, people usually spend a long time there"
 
-        if self.consequents['fast food'] is None:
-            self.consequents['fast food'] = True if cheap and international else None
-            if self.consequents['fast food']:
+        if consequents['fast food'] is None or 1 > self.priorities['fast food']:
+            new_consequents['fast food'] = True if cheap and international else new_consequents['fast food']
+            if cheap and international:
+                self.priorities['fast food'] = 1
                 self.reason['fast food'] = "The restaurant serves fast food, as it is cheap and international"
 
-        if self.consequents['vegetarian'] is None:
-            self.consequents['vegetarian'] = False if steakhouse else None
-            if self.consequents['vegetarian'] is False:
+        if consequents['vegetarian'] is None or 1 > self.priorities['vegetarian']:
+            new_consequents['vegetarian'] = False if steakhouse else new_consequents['vegetarian']
+            if steakhouse:
+                self.priorities['vegetarian'] = 1
                 self.reason['vegetarian'] = "As the restaurant is a steakhouse, it is not vegetarian"
 
-        if self.consequents['romantic'] is None:
-            self.consequents['romantic'] = False if self.consequents['fast food'] else None
-            if self.consequents['romantic'] is False:
+        if consequents['romantic'] is None or 3 > self.priorities['romantic']:
+            new_consequents['romantic'] = False if consequents['fast food'] is True else new_consequents['romantic']
+            if consequents['fast food'] is True:
+                self.priorities['romantic'] = 3
                 self.reason['romantic'] = "The restaurant serves fast food, therefore, it is not romantic"
 
-        if self.consequents['children'] is None:
-            self.consequents['children'] = True if self.consequents['fast food'] else None
-            if self.consequents['children']:
+        if consequents['children'] is None or 1 > self.priorities['children']:
+            new_consequents['children'] = True if consequents['fast food'] is True else new_consequents['children']
+            if consequents['fast food'] is True:
+                self.priorities['children'] = 1
                 self.reason['children'] = "The restaurant serves fast food, therefore, children like it"
 
-        if self.consequents['busy'] is None:
-            self.consequents['busy'] = True if self.consequents['vegetarian'] and cheap else None
-            if self.consequents['busy']:
-                self.reason['busy'] = "The restaurant serves busy, because it serves cheap vegetarian food"
+        if consequents['busy'] is None or 2 > self.priorities['busy']:
+            new_consequents['busy'] = True if consequents['vegetarian'] is True and cheap else new_consequents['busy']
+            if consequents['vegetarian'] is True and cheap:
+                self.priorities['busy'] = 2
+                self.reason['busy'] = "The restaurant is busy, because it serves cheap vegetarian food"
 
+        return new_consequents
+
+    def solve_rule(self, restaurant):
+        # set consequents
+        consequents = {
+            'busy': None,
+            'long time': None,
+            'children': None,
+            'romantic': None,
+            'fast food': None,
+            'vegetarian': None
+        }
+
+        not_changed = False
+
+        # loop until new consequents are the same as previous consequents (closure under implication)
+        while not_changed is False:
+            new_consequents = self.__solve_rule(restaurant, consequents)
+            if new_consequents == consequents:
+                # quit if no change happened in this iteration
+                not_changed = True
+            else:
+                # otherwise update consequents
+                consequents = new_consequents
+
+        self.consequents = new_consequents
         return self.consequents, self.reason
-
-    # def solve_rule(self, rule, expected_result, restaurants):
-    #     results = []
-    #     for restaurant in restaurants:
-    #         consequents, reason = self.__solve_rule(restaurant)
-    #         results.append((consequents, reason))
 
 
 if __name__ == '__main__':
@@ -152,11 +200,11 @@ if __name__ == '__main__':
     # rule.set_rules()
     # restaurant = {"restaurantname": "pizza express", "pricerange": "cheap", "area": "centre",
     #               "food": "spanish", "phone": "01223 324033", "addr": "", "post": "c.b 2", "foodquality": "moderate"}
-    restaurant = {"restaurantname": "steakhouse express", "pricerange": "cheap", "area": "centre",
-                  "food": "international", "phone": "01223 324033", "addr": "", "post": "c.b 2", "foodquality": "moderate"}
+    restaurant = {"restaurantname": "pizza express", "pricerange": "expensive", "area": "centre",
+                  "food": "spanish", "phone": "01223 324033", "addr": "", "post": "c.b 2", "foodquality": "good"}
 
-    restaurant = pd.DataFrame(restaurant, index=[0])
     print(rule.consequents)
-    answer, reason = rule.solve_rule('romantic', restaurant)
+    answer, reason = rule.solve_rule(restaurant)
     print(answer)
     print(reason)
+    print(rule.priorities)
